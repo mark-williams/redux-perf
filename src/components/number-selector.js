3d35c9b3858e1521fddff1d360dc6489af0ea621
mark-williams <markwill@hotmail.co.uk>
2016-10-02 17:38:37 +0100

    Styling (of sorts!)

--- src/components/number-selector.js
+++ src/components/number-selector.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 const NumberSelector = (props) => {
     return (
-            <div>
-                <input type="radio" name="numberTypes" value="odd" onClick={ props.onChoiceChange.bind(this) } />Odd<br />
-                <input type="radio" name="numberTypes" value="even" onClick={ props.onChoiceChange.bind(this) } />Even<br />
-                <input type="radio" name="numberTypes" value="primes" onClick={ props.onChoiceChange.bind(this) } />Primes<br />
-                <input type="radio" name="numberTypes" value="fibonacci" onClick={ props.onChoiceChange.bind(this) } />Fibonacci<br />
+            <div className="number-selector">
+                <ul>
+                    <li><input type="radio" name="numberTypes" value="odd" onClick={ props.onChoiceChange.bind(this) } />Odd</li>
+                    <li><input type="radio" name="numberTypes" value="even" onClick={ props.onChoiceChange.bind(this) } />Even</li>
+                    <li><input type="radio" name="numberTypes" value="primes" onClick={ props.onChoiceChange.bind(this) } />Primes</li>
+                    <li><input type="radio" name="numberTypes" value="fibonacci" onClick={ props.onChoiceChange.bind(this) } />Fibonacci</li>
+                </ul>
             </div>
         );
 };
